9e4cbdd0b11d7ec56816c0747538f59dcd5525fd
demeritcowboy <demeritcowboy@hotmail.com>
2025-05-11 12:03:31 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# SUMMARY
+
+Entity Print doesn't let you just print, you have to create and open a pdf first.
+
+It feels though like this should be a theme template instead, not an entity
+print plugin.
+
+# REQUIREMENTS
+
+* Entity Print
---
+++ src/Plugin/EntityPrint/PrintEngine/PrintPrintEngine.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\entity_print_print\Plugin\EntityPrint\PrintEngine;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\entity_print\Plugin\ExportTypeInterface;
+use Drupal\entity_print\Plugin\PrintEngineBase;
+
+/**
+ * Print Print plugin implementation.
+ *
+ * @PrintEngine(
+ *   id = "printprintengine",
+ *   label = @Translation("PrintPrint"),
+ *   export_type = "pdf"
+ * )
+ */
+class PrintPrintEngine extends PrintEngineBase {
+
+  /**
+   * @var string
+   */
+  protected $html = '';
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, ExportTypeInterface $export_type) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $export_type);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function addPage($content) {
+    $this->html .= (string) $content;
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function send($filename, $force_download = TRUE) {
+    header("Cache-Control: private");
+    header("Content-Type: text/html");
+    echo $this->html;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getBlob() {
+    return $this->html;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function dependenciesAvailable() {
+    return TRUE;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPrintObject() {
+    return NULL;
+  }
+
+}
